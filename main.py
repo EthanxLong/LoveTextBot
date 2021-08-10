098e1685cab60576c085008403cb367d6467258a
Ethan Long <55002363+EthanxLong@users.noreply.github.com>
2021-08-10 00:44:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 from msg import (msg)
 
-account_sid = 'ACd673c80d33de9debb49a6342f5084463'
-auth_token = 'a1fbb4c6b7e2cf838e631fd80c633d33'
+account_sid = 'ACd673c80d33-------'
+auth_token = 'a1fbb4c6b7e2c-------'
 
 client = Client(account_sid, auth_token)
 
